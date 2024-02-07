8bb745d4c72251f68d22bd75111fe235880124ec
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-08 01:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if (str.length === 0) {
+        return null;
+    }
+
+  
+    for (let i = 0; i < str.length; i++) {
+        let nonRepeat = true;
+        let currentChar = str.charAt(i);
+ // Check if the current character is repeated in the string
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j && currentChar === str.charAt(j)) {
+                nonRepeat = false;
+                break;
+            }
+        }
+
+       
+        if (nonRepeat) {
+            return currentChar;
+        }
+    }
+
+    
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
